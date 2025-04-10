393e639061c0fb48b3f7ab0b951898ffb5b97ed1
hasan75 <hasanjab14@gmail.com>
2025-04-11 02:07:01 +0600

    fix(frontend): change about page texts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function About() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <h1>Kuddus</h1>
+            <h2>Task Tracker Application by Hasan Ahmed</h2>
         </div>
     );
 }
